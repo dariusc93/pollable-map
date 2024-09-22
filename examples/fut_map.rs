6dae857a694d0f2bfc5290dd2dc9ad85c0d866f9
Darius <darius.clark@proton.me>
2024-09-22 10:16:12 -0400

    chore: update example

--- examples/fut_map.rs
+++ examples/fut_map.rs
@@ -1,17 +1,23 @@
 use futures::future::BoxFuture;
-use futures::{ StreamExt};
+use futures::StreamExt;
 use futures_map::futures::FutureMap;
 
 fn main() {
     futures::executor::block_on(async {
         let mut fut_map = FutureMap::<u8, BoxFuture<'static, ()>>::new();
 
-        fut_map.insert(1, Box::pin(async {
-            println!("message from 1");
-        }));
-        fut_map.insert(2, Box::pin(async {
-            println!("message from 2");
-        }));
+        fut_map.insert(
+            1,
+            Box::pin(async {
+                println!("message from 1");
+            }),
+        );
+        fut_map.insert(
+            2,
+            Box::pin(async {
+                println!("message from 2");
+            }),
+        );
 
         while let Some((key, _)) = fut_map.next().await {
             println!("> future \"{}\" is finished", key);
---
+++ examples/st_map.rs
@@ -0,0 +1,30 @@
+use futures::stream::BoxStream;
+use futures::StreamExt;
+use futures_map::stream::StreamMap;
+
+fn main() {
+    futures::executor::block_on(async {
+        let mut st_map = StreamMap::<u8, BoxStream<'static, ()>>::new();
+
+        st_map.insert(
+            1,
+            futures::stream::once(async {
+                println!("message from 1");
+            })
+            .boxed(),
+        );
+        st_map.insert(
+            2,
+            futures::stream::once(async {
+                println!("message from 2");
+            })
+            .boxed(),
+        );
+
+        while let Some((key, _)) = st_map.next().await {
+            println!("> future \"{}\" is finished", key);
+        }
+
+        println!("Done processing map")
+    });
+}
