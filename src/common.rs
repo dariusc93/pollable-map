0719aa7c90c9a5e14fd87443e0228078245c4bdd
Darius <darius.clark@proton.me>
2025-07-11 09:18:56 -0500

    chore: remove redundant reference

--- src/common.rs
+++ src/common.rs
@@ -77,14 +77,13 @@ where
     type Output = (K, Option<S::Output>);
 
     fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-        let this = &mut *self;
-        let Some(st) = this.inner_pin() else {
-            return Poll::Ready((this.key.clone(), None));
+        let Some(st) = self.inner_pin() else {
+            return Poll::Ready((self.key.clone(), None));
         };
 
         let output = futures::ready!(st.poll(cx));
-        this.inner.take();
-        Poll::Ready((this.key.clone(), Some(output)))
+        self.inner.take();
+        Poll::Ready((self.key.clone(), Some(output)))
     }
 }
 
@@ -96,8 +95,7 @@ where
     type Item = (K, Option<S::Item>);
 
     fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
-        let this = &mut *self;
-        let Some(st) = this.inner_pin() else {
+        let Some(st) = self.inner_pin() else {
             // Note: While we could panic for any attempts to poll the stream that doesnt exist or have been terminated,
             //       we opt to just return `Poll::Ready(None)` and letting upstream define how to handle a terminated stream
             //       although in the future this could change as we should not be polling any terminated streams or futures.
@@ -106,21 +104,21 @@ where
 
         match st.poll_next(cx) {
             Poll::Ready(Some(value)) => {
-                if this.wake_on_success {
+                if self.wake_on_success {
                     // Since we made progress, we should attempt to proceed further by waking up the task
                     // TODO: Find a better way to wake task up without needing to call the waker on every successful result
                     //       from stream
                     cx.waker().wake_by_ref();
                 }
-                Poll::Ready(Some((this.key.clone(), Some(value))))
+                Poll::Ready(Some((self.key.clone(), Some(value))))
             }
             Poll::Ready(None) => {
                 // Note: Although some streams can return a `Poll::Ready(None)`, we will have to assume that the stream is completely finished
                 //       and terminated at this point and that we should not attempt to poll again.
                 //       In the future, we could probably provide a flag that would allow us to take the inner stream or keep it and attempt on polling it again
                 //       without actually terminating it.
-                this.inner.take();
-                Poll::Ready(Some((this.key.clone(), None)))
+                self.inner.take();
+                Poll::Ready(Some((self.key.clone(), None)))
             }
             Poll::Pending => Poll::Pending,
         }
