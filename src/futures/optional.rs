be2cb832d458c1c3676dbf6905b16b0940fa07da
Darius Clark <darius.clark@proton.me>
2026-03-28 20:03:28 -0400

    chore: misc correction

--- src/futures/optional.rs
+++ src/futures/optional.rs
@@ -2,7 +2,7 @@ use crate::optional::Optional;
 
 /// A reusable future that is the equivalent to an `Option`.
 ///
-/// By default, this future will be empty, which would return  [`Poll::Pending`] when polled,
+/// By default, this future will be empty, which would return [`Poll::Pending`] when polled,
 /// but if a [`Future`] is supplied either upon construction via [`OptionalFuture::new`] or
 /// is set via [`OptionalFuture::replace`], the future would then be polled once [`OptionalFuture`]
 /// is polled. Once the future is polled to completion, the results will be returned, with
--- src/stream/optional.rs
+++ src/stream/optional.rs
@@ -2,9 +2,9 @@ use crate::optional::Optional;
 
 /// A reusable stream that is the equivalent to an `Option`.
 ///
-/// By default, this future will be empty, which would return  [`Poll::Pending`] when polled,
+/// By default, this future will be empty, which would return [`Poll::Pending`] when polled,
 /// but if a [`Stream`] is supplied either upon construction via [`OptionalStream::new`] or
-/// is set via [`OptionalStream::replace`], the stream could later polled once [`OptionalStream`]
+/// is set via [`OptionalStream::replace`], the stream could later poll once [`OptionalStream`]
 /// is polled. Once the stream is polled to completion, [`OptionalStream`] will be empty.
 #[deprecated(note = "Use Optional instead")]
 pub type OptionalStream<S> = Optional<S>;
